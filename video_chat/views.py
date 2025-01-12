c8ac14c1c3dfb6c1619ced6e3a0fa6ecaf2cffb7
SubhamSaha9 <sahasubham7478@gmail.com>
2025-01-13 03:49:19 +0530

    register added

--- video_chat/views.py
+++ video_chat/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'auth/login.html', {'success': "Registration successful. Please login."})
+        else:
+            error_message = form.errors.as_text()
+            return render(request, 'auth/signup.html', {'error': error_message})
+
+    return render(request, 'auth/signup.html')
 
