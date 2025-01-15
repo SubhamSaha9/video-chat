ffaff7947f344bd42dbfed8ee2b4560d9fad4a9f
SubhamSaha9 <sahasubham7478@gmail.com>
2025-01-15 22:01:14 +0530

    dashboard route created

--- video_chat/urls.py
+++ video_chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/',views.login_view, name='login'),
+    path('dashboard/',views.dashboard, name='dashboard'),
     path('',views.index, name='index'),
 ]
