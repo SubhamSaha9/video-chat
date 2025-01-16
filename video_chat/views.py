3f75853f7d0b98a64f4d12834782c6c60a31f9af
SubhamSaha9 <sahasubham7478@gmail.com>
2025-01-17 02:44:55 +0530

    join room view

--- video_chat/views.py
+++ video_chat/views.py
@@ -42,3 +42,9 @@ def logout_view(request):
     logout(request)
     return redirect("/login")
 
+@login_required
+def join_room(request):
+    if request.method == 'POST':
+        roomID = request.POST['roomID']
+        return redirect("/meeting?roomID=" + roomID)
+    return render(request, 'dashboard/joinroom.html')
